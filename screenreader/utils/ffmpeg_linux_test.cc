2a794b0a651af2caf478e6593a2aed33e150e229
d61h6k4 <ddbihbka@gmail.com>
2024-05-27 13:10:17 +0200

    Add tests for linux platform

---
+++ screenreader/utils/ffmpeg_linux_test.cc
@@ -0,0 +1,13 @@
+
+#include "screenreader/utils/ffmpeg.h"
+#include "gtest/gtest.h"
+
+TEST(TestFFmpegUtils, CheckCaptureAudio) {
+    auto res = aikit::utils::CaptureDevice("pulse", "default");
+    EXPECT_TRUE(res.ok()) << res.message();
+}
+
+TEST(TestFFmpegUtils, CheckCaptureScreen) {
+    auto res = aikit::utils::CaptureDevice("x11grab", ":0.0+100,200");
+    EXPECT_TRUE(res.ok()) << res.message();
+}
