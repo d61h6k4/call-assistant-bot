0a4a1e98575530b7bb5e39e6909f38e7ce17e399
d61h6k4 <ddbihbka@gmail.com>
2024-06-02 18:42:48 +0200

    Fix av_err2str for GCC

--- screenreader/utils/audio.cc
+++ screenreader/utils/audio.cc
@@ -14,6 +14,20 @@ extern "C" {
 }
 #endif
 
+
+// Replacement of av_err2str, which causes
+// `error: taking address of temporary array`
+// https://github.com/joncampbell123/composite-video-simulator/issues/5
+#ifdef av_err2str
+#undef av_err2str
+#include <string>
+av_always_inline std::string av_err2string(int errnum) {
+    char str[AV_ERROR_MAX_STRING_SIZE];
+    return av_make_error_string(str, AV_ERROR_MAX_STRING_SIZE, errnum);
+}
+#define av_err2str(err) av_err2string(err).c_str()
+#endif  // av_err2str
+
 namespace aikit::media {
 
 absl::StatusOr<AudioFrame>
