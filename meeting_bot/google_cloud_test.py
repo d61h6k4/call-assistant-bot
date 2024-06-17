a1d9d6dc8789a6dce6cdf2a021d6b15a9bfdeda1
d61h6k4 <ddbihbka@gmail.com>
2024-06-17 13:34:17 +0200

    Fix issue with different version of protobuf
    Packages grpcio and google-cloud-storage uses different imcompartable
    versions for protobuf

--- meeting_bot/google_cloud_test.py
+++ meeting_bot/google_cloud_test.py
@@ -1,11 +1,17 @@
 import unittest
 
+from pathlib import Path
 from google.auth import default
+from meeting_bot.google_cloud import upload_blob
 
 
-def test_auth():
-    # Check that it doesn't raise exception
-    default()
+class TestGoogleCoud(unittest.TestCase):
+    def test_auth(self):
+        # Check that it doesn't raise exception
+        default()
+
+    def test_upload_blob(self):
+        upload_blob(Path("testdata/testvideo.mp4"), "2024/06/17/testvideo.mp4")
 
 
 if __name__ == "__main__":
